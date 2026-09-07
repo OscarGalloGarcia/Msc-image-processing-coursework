a71a0a867aeb92c17f09df99ca9475441309eb8e
Oscar Alberto Gallo García <oscar.gallo@inbest.cloud>
2026-09-06 21:06:24 -0600

    refactor folder structures and names

---
+++ Practicas/p2-histograma.py
@@ -0,0 +1,13 @@
+#Actividad: Calcular el histograma de una imagen.
+
+# 1) I_org -> Imagen original de entrada.
+# 2) Hist_org -> Histograma de la imagen original.
+# 3) Histogramas por especificacion definidos por funciones.
+#    Definir uno para cada canal RGB
+# 4) I_out -> Mostrar imagen resultante
+# 5) Hist_out -> Mostrar el histograma de la imagen resultante.
+
+import numpy as np
+import cv2 
+
+I_org = cv2.imread()
